0bb07c6ffa6bc0a7b11864b306847ea2157fe862
Lucas Feliciano <lucas.feliciano@bry.com.br>
2019-10-03 16:34:27 -0300

    Inicia parte mobile do projeto

---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+import logo from '../assets/logo.png'
+
+export default function Login() {
+    return <View>
+            <Image source={logo} />
+        </View>
+}
